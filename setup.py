a8f964a4c07b054ee892d0941d274ed91a6ea770
Leigh Johnson <leigh@bitsy.ai>
2023-01-09 15:26:45 -0800

    revert back to octoprint_setuptools

--- setup.py
+++ setup.py
@@ -24,15 +24,13 @@ additional_setup_parameters = {}
 ########################################################################################################################
 
 try:
-    from octoprint_nanny.vendor import octoprint_setuptools
-except Exception as e:
-    print(
-        "Could not import OctoPrint's setuptools, are you sure you are running that under "
-        "the same python installation that OctoPrint is installed under?"
-    )
+    import octoprint_setuptools
+except ImportError:
+    print("Could not import OctoPrint's setuptools, are you sure you are running that under "
+          "the same python installation that OctoPrint is installed under?")
     import sys
-
     sys.exit(-1)
+    
 
 setup_parameters = octoprint_setuptools.create_plugin_setup_parameters(
     identifier=plugin_identifier,
